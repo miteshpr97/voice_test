dd4eff9459252ea1e039a54fda68a475b5bd17ae
miteshpr97 <miteshpradhan97@gmail.com>
2025-07-19 14:26:06 +0530

    change username/login

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
-  const [email, setEmail] = useState('demo@gmail.com');
+  const [email, setEmail] = useState('demo');
   const [password, setPassword] = useState('demo');
   const navigate = useNavigate();
 
